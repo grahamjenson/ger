3e9e410f236b9123b660e5fdf152455154902761
Graham Jenson <grahamjenson@maori.geek.nz>
2015-07-09 14:52:14 +0100

    deleteing the node modules dir

--- node_modules/knex/knex.js
+++
@@ -1,8 +0,0 @@
-// Knex.js
-// --------------
-//     (c) 2014 Tim Griesser
-//     Knex may be freely distributed under the MIT license.
-//     For details and documentation:
-//     http://knexjs.org
-
-module.exports = require('./lib/index')
--- node_modules/pg-copy-streams/test/index.js
+++
@@ -1,3 +0,0 @@
-require('./copy-from')
-require('./copy-to')
-require('./binary')
